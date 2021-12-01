25288816f1da15bfb75820fb305e2df15968561d
valik94 <valentin.lyashenko@ryerson.ca>
2021-12-01 04:31:22 +0000

    initial commit for cats including all test cases and stretch exercise

---
+++ test/syncBreedsTest.js
@@ -0,0 +1,20 @@
+const assert = require('chai').assert;
+const breedDetails = require('../syncBreeds');
+
+describe('#breedDetails', () => {
+  it('returns breed details for the Bombay breed', () => {
+    // We expect it to be a function, but will it be?
+    //console.log(breedDetails);
+    const expectedBombay = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+
+    // get the return value right away from the function
+const bombay = breedDetails('Bombay');
+assert.equal(expectedBombay, bombay);
+  }),
+
+  it("returns breed details for the Balinese breed", () =>{
+    const expectedBalinese = "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.";
+    const balinese = breedDetails('Balinese');
+assert.equal(expectedBalinese, balinese );    
+  })
+});
